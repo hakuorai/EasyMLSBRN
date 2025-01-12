a5d2f2cb23e99c13930d0af9d115c4c9a7ca662b
hakuorai <hakuorai@outlook.com>
2025-01-12 17:45:48 +0900

    remove README pics from original MLS-BRN

--- configs/loft_foahfm_ssl/loft_foahfm_r50_fpn_1x_bonai_ssl_chengdu.py
+++ configs/loft_foahfm_ssl/loft_foahfm_r50_fpn_1x_bonai_ssl_chengdu.py
@@ -1,6 +1,6 @@
 _base_ = [
     "../_base_/models/bonai_loft_foahfm_r50_fpn_basic.py",
     "../_base_/datasets/bonai_instance_hfm_ssl_chengdu.py",
-    "../_base_/schedules/schedule_1x.py",
-    "../_base_/default_runtime.py",
+    "../_base_/schedules/schedule_1x_bonai.py",
+    "../_base_/default_runtime.py"
 ]
